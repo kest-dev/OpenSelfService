0891cc8cdb213fafbdfc7baa3302257db6176371
Kest <meowtss@protonmail.com>
2024-07-03 12:08:19 -0400

    Adding project files

---
+++ Log/Log.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+
+#define LOG_DEBUG(message) \
+    std::cout << "[[ DEBUG ]] " << message << std::endl;
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Log/Log.h
+
+int main()
+{
+    return 0;
+}
